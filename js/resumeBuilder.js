a1c97b2caaa943c98a9055fabb3ab4d4b6119527
stanislava-1 <stanislava-1@users.noreply.github.com>
2016-11-11 11:53:26 +0100

    first commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,34 @@
+// var projects = [
+// 	{
+// 		"name": "Castles and Manor Houses in Slovakia",
+// 		"technologies": "HTML, CSS, Javascript, jQuery (ajax, getJSON), KnockoutJS, Google Maps API, WikiMedia action API, OpenWeatherMap API",
+// 		"description" : "A map with markers on places where castles and manor houses of Slovakia are. Each marker has an infowindow with some information about the castle/manor house including links for wikipedia articles (using WikiMedia action API, AJAX) and weather forecast (using OpenWeatherMap API, JSON). There is also a possibility to filter the markers and search for particular castle/manor house.",
+// 		"date": [2016, 6],
+// 		"url": "http://www.kocalkova.sk/castles/"
+// 	},
+// 	{
+// 		"name": "Game: Reach the Water",
+// 		"technologies": "HTML, HTML5 – Canvas, CSS, Javascript, jQuery",
+// 		"description" : "A game inspired by the classic arcade game Frogger. I developed it after finishing two Udacity online courses - Object Oriented JavaScript and HTML5 Canvas - to check out my knowledge of these two technologies.",
+// 		"date": [2016, 1],
+// 		"url": "http://www.kocalkova.sk/reach-the-water/"
+// 	},
+// 	{
+// 		"name": "Word Count",
+// 		"technologies": "HTML, CSS, Python, Jinja2, Google App Engine",
+// 		"description" : "A project assigned by myself to test my ability to code in Python and use Jinja2 after finishing the Udacity online course Intro to Programming. It is a web application that takes a text and produces an analysis of the text: occurrence of different words (number and percentage), total number of words, number of different words and number of words that create 80% of the text.",
+// 		"date": [2015, 11],
+// 		"url": "http://word-count-1119.appspot.com/"
+// 	},
+// 	{
+// 		"name": "Intro to Programming",
+// 		"technologies": "HTML, CSS, Python, Jinja2, Google App Engine, (a bit of Javascript)",
+// 		"description" : "Final project of the Udacity Intro to Programming online course. The purpose of this project was to present the HTML and CSS knowledge, Python knowledge, using Jinja2 templates and Google App Engine Datastore (for saving comments).",
+// 		"date": [2015, 10],
+// 		"url": "http://nomadic-sublime-107616.appspot.com/"
+// 	}
+// ];
+
+
+
+
